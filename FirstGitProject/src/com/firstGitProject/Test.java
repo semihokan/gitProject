b964500da18ae226431744a5e9b3a0788c57b24c
Semih Okan Pehlivan <pehlivansemihokan@gmail.com>
2014-12-25 00:14:12 +0200

    2. commit

--- FirstGitProject/src/com/firstGitProject/Test.java
+++ FirstGitProject/src/com/firstGitProject/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First GIT project");
+		System.out.println("test2");
 	}
 
 }
