3727b18944c34e4b826afdf39726758b26b01eaf
Semih Okan <semihokanp@gmail.com>
2014-12-25 01:32:34 +0200

    2.pc

--- FirstGitProject/src/com/firstGitProject/Test.java
+++ FirstGitProject/src/com/firstGitProject/Test.java
@@ -5,8 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First GIT project");
-		System.out.println("1 deðiþtirdi");
-		System.out.println("1 deðiþtirdi");
+		System.out.println("2.pc");
+		System.out.println("1 deï¿½iï¿½tirdi");
 		System.out.println("second pc test");
 		System.out.println("second pc test");
 	}
