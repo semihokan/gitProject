f73b1646ae96d33512c14acdc2e89a2cfa434c0d
Semih Okan <semihokanp@gmail.com>
2014-12-25 00:16:16 +0200

    2. pc change

--- FirstGitProject/src/com/firstGitProject/Test.java
+++ FirstGitProject/src/com/firstGitProject/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("First GIT project");
 		System.out.println("test2");
+		System.out.println("second pc test");
 	}
 
 }
