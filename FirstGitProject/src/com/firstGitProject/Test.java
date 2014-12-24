d17e10e7c581037d5b20e02822beb2cc57ecbd59
Seda <sedapehli@gmail.com>
2014-12-25 00:23:49 +0200

    8. satır

--- FirstGitProject/src/com/firstGitProject/Test.java
+++ FirstGitProject/src/com/firstGitProject/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First GIT project");
-		System.out.println("test2");
+		System.out.println("1 deðiþtirdi");
 		System.out.println("second pc test");
 	}
 
